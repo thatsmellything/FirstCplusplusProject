e0ea089dc0ce42af97259e592b0c3bf3d95ec870
Jensen Judkins <thesmelliestman@gmail.com>
2019-01-22 08:56:22 -0700

    Initial commit

---
+++ FirstCplusplusProject/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  FirstCplusplusProject
+//
+//  Created by Judkins, Jensen on 1/22/19.
+//  Copyright © 2019 Judkins, Jensen. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    // colon colon means scope resolution, so that the c operation lives inside the standard library. Basically data member. << is the output stream indicator, aka stuff on the right side puts that stuff on the output. >> are for input.
+    std::cout << "Back to C++! Hopefully I remember my stuff from two years ago.\n";
+    return 0;
+}
